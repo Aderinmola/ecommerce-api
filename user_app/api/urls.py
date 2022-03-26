e788a6cc98acf3d5afb417f143ecd0c796af585d
Boboye🌴 <boboye@Boboyes-MacBook.local>
2022-03-26 20:32:29 +0100

    swagger integration

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from user_app.api.views import register_view, logout_view
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 
 urlpatterns = [
+     # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     #path('login/', obtain_auth_token, name='login'),
     path('register/',register_view, name='register'),
     path('logout/',logout_view, name='logout'),
